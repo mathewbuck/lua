78aa3c85e4602270a6490f6078e06e1a0f84c48f
Mathew <mathewbuck38@gmail.com>
2022-06-04 16:09:41 -0700

    windows sleep(x) function

---
+++ Wsleep.lua
@@ -0,0 +1,13 @@
+--[[
+-- Simple function that will pause for x seconds
+-- This is OS specific - Windows
+--]]
+
+local function Wsleep(s)
+	os.execute("timeout " .. tonumber(s) .. " /nobreak >nul")
+end
+
+print("Hello")
+Wsleep(2)
+print("Two second pause")
+
