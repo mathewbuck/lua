83513a6307c4e6ac8e06c1d5feb7d83ed967c867
Mathew <mathewbuck38@gmail.com>
2022-06-04 20:00:26 -0800

    Update & rename

---
+++ fish.lua
@@ -0,0 +1,38 @@
+--[[
+--*****************************************************************************************************
+--*      Small module to add Unix 'clear' & 'sleep(x)' functions to Lua scripts                       *
+--*      Module will auto detect the user's system, if Unix/Linux/MacOS/Android{Termux}               *
+--*               OR                                                                                  *
+--*             Windows                                                                               *
+--*      sleep(x)  x-is always in seconds                                                             *
+--*****************************************************************************************************
+--]]
+
+
+local fish = {}
+
+usrOS = (package.config:sub(1,1))
+
+if usrOS == '/' then
+	
+	function fish.clear()
+		os.execute('clear')
+		end
+	
+	function fish.sleep(n)
+		os.execute('sleep ' .. tonumber(n))
+		end
+	else
+	
+	function fish.clear()
+		os.execute('cls')
+	end
+
+	function fish.sleep(s)
+		os.execute('timeout ' .. tonumber(s) .. ' /nobreak >nul')
+	end
+end
+	
+	
+return fish
+
